691db234e821510420b9a3ee9b9d1a70b6a2a1b7
virtu <tomliau33@gmail.com>
2019-12-05 23:31:09 +0800

    Add G0vProjectList Page

---
+++ G0vProjectList/precache-manifest.6d2e30e43b14ec28a8cfde9d14a0d04f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778feb67c78bd4d7c9b4",
+    "url": "/css/app.c77ebbbd.css"
+  },
+  {
+    "revision": "23611446d4735bf4ad1f9578b3a44b4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "055704ea7c7e678c1da8",
+    "url": "/js/about.cfbb28e1.js"
+  },
+  {
+    "revision": "778feb67c78bd4d7c9b4",
+    "url": "/js/app.8dcfbb56.js"
+  },
+  {
+    "revision": "2b16875bb5491bfcfcb3",
+    "url": "/js/chunk-vendors.854262ae.js"
+  },
+  {
+    "revision": "efbae9010f0cf9d2a02aaa7c9a28da14",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ G0vProjectList/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.6d2e30e43b14ec28a8cfde9d14a0d04f.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "g0vproject"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
